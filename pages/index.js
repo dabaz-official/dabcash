82f3fdb466051b63d403f9fbf5939693b824cd26
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-11-24 03:36:17 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import BackgroundSvg from '../components/BackgroundSvg'
 import Navbar from '../components/Navbar'
 import { BoltIcon, DevicePhoneMobileIcon, GlobeAltIcon, CreditCardIcon } from '@heroicons/react/24/outline'
+import { CheckCircleIcon } from '@heroicons/react/24/solid'
 import Footer from '../components/Footer'
 import GetStarted from '../components/GetStarted'
 
@@ -30,6 +31,13 @@ const features = [
       "Even if it's a web application, you can also use it on your iPhone or iPad. We made perfect responsive design for our customers.",
     icon: DevicePhoneMobileIcon,
   },
+]
+
+const premiumFeatures = [
+  'End-to-end encryption',
+  'Add unlimited cards',
+  'Add unlimited accounts',
+  'Connect with another account',
 ]
 
 export default function Home() {
@@ -136,6 +144,77 @@ export default function Home() {
                     </div>
                   </div>
                 ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="pricing">
+          <div className="pt-12 sm:pt-16 lg:pt-20">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="sm:text-center">
+                <h2 className="text-lg font-semibold leading-8 text-violet-600">Pricing</h2>
+                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Simple no-extra pricing</p>
+                <p className="mx-auto mt-6 max-w-2xl text-lg leading-8 text-gray-600">
+                  If you're not satisfied with our premium membership, contact us within the first 14 days and we'll send you a full refund.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-8 bg-gray-50 pb-16 sm:mt-12 sm:pb-20 lg:pb-28">
+            <div className="relative">
+              <div className="absolute inset-0 h-1/2 bg-white" />
+              <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="max-w-lg mx-auto rounded-lg shadow-lg overflow-hidden lg:max-w-none lg:flex">
+                  <div className="flex-1 bg-white px-6 py-8 lg:p-12">
+                    <h3 className="text-2xl font-extrabold text-gray-900 sm:text-3xl">Lifetime Premium Membership</h3>
+                    <p className="mt-6 text-base text-gray-500">
+                      With our premium membership, you can use DabCash with unlimited features.
+                    </p>
+                    <div className="mt-8">
+                      <div className="flex items-center">
+                        <h4 className="flex-shrink-0 pr-4 bg-white text-sm tracking-wider font-semibold uppercase text-violet-600">
+                          What's included
+                        </h4>
+                        <div className="flex-1 border-t-2 border-gray-200" />
+                      </div>
+                      <ul role="list" className="mt-8 space-y-5 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-5">
+                        {premiumFeatures.map((feature) => (
+                          <li key={feature} className="flex items-start lg:col-span-1">
+                            <div className="flex-shrink-0">
+                              <CheckCircleIcon className="h-5 w-5 text-green-400" aria-hidden="true" />
+                            </div>
+                            <p className="ml-3 text-sm text-gray-700">{feature}</p>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="py-8 px-6 text-center bg-gray-50 lg:flex-shrink-0 lg:flex lg:flex-col lg:justify-center lg:p-12">
+                    <p className="text-lg leading-6 font-medium text-gray-900">Pay once, own it forever</p>
+                    <div className="mt-4 flex items-center justify-center text-5xl font-extrabold text-gray-900">
+                      <span>$9.99</span>
+                      <span className="ml-3 text-xl font-medium text-gray-500">USD</span>
+                    </div>
+                    <p className="mt-4 text-sm">
+                      With a 7-days free trial
+                    </p>
+                    <div className="mt-6">
+                      <div className="rounded-md shadow">
+                        <a
+                          href="#"
+                          className="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-xl text-white bg-violet-600 hover:bg-violet-700 transition"
+                        >
+                          Get Access
+                        </a>
+                      </div>
+                    </div>
+                    <div className="mt-4 text-sm">
+                      <a href="#" className="font-medium text-gray-900">
+                        Membership policy
+                      </a>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
